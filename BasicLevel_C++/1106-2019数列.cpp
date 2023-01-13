7b0aaf65646f577bc39d556c64d9323869102d26
Ihtml <2483690020@qq.com>
2023-01-13 09:18:03 +0800

    feat(pat-1106): optimize: array cache number

--- BasicLevel_C++/1106-2019数列.cpp
+++ BasicLevel_C++/1106-2019数列.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main() {
+    int n, sum, A[4] = {2, 0, 1, 9};
+    cin >> n;
+    for (int i = 0; i < min(4, n); i++)
+        cout << A[i];
+    for (int i = 4; i < n; i++) {
+        sum = (A[0] + A[1] + A[2] + A[3]) % 10;
+        cout << sum;
+        A[0] = A[1];
+        A[1] = A[2];
+        A[2] = A[3];
+        A[3] = sum;
+    }
+    return 0;
+}
+
+#include <vector>
+int main1() {
     int n, p = 4;
     vector<int> A(4);
     A[0] = 2;
