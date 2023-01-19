04721470fe1b0c40eecfd9206fc55d579921f2fd
Ihtml <2483690020@qq.com>
2023-01-19 21:15:10 +0800

    feat(pat-1108): optimize

--- BasicLevel_C++/1108-String复读机-字符和字符串处理.cpp
+++ BasicLevel_C++/1108-String复读机-字符和字符串处理.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 int main() {
+    vector<int>  c(200);
+    int flag = 1;
+    string s = "String", SS;
+    cin >> SS;
+    for (auto it : SS)
+        c[it]++;
+    while (flag) {
+        flag = 0;
+        for (int i = 0; i < 6; i++) {
+            if (c[s[i]] != 0) {
+                cout << s[i];
+                flag = 1;
+                c[s[i]]--;
+            }
+        }
+    }
+    return 0;
+}
+int main1() {
     string s = "String", SS;
     cin >> SS;
     unordered_map<char, int> mp;
-    int l=s.length(), len = SS.length();
+    int l = s.length(), len = SS.length();
     for (int i = 0; i < l; i++) {
         mp[s[i]] = 0;
     }
