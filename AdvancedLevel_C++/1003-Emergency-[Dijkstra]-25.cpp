edaf53b777fbecc96eca256103accd64cb29b00d
Ihtml <2483690020@qq.com>
2023-02-01 09:59:38 +0800

    feat(pat-a-1103): temporarily commit

--- AdvancedLevel_C++/1003-Emergency-[Dijkstra]-25.cpp
+++ AdvancedLevel_C++/1003-Emergency-[Dijkstra]-25.cpp
@@ -16,9 +16,9 @@ int main() {
         scanf("%d%d%d", &a, &b, &c);
         e[a][b] = e[b][a] = c;
     }
-    dis[c1] = 0;
+    dis[c1] = 0;   // dis[i]表示从出发点到i点最短路径的长度
     w[c1] = weight[c1];
-    num[c1] = 1;
+    num[c1] = 1;   // num[i]表示从出发点到i结点最短路径的条数
     for (int i = 0; i < n; i++) {
         int u = -1, minn = inf;
         for (int j = 0; j < n; j++) {
@@ -31,7 +31,20 @@ int main() {
             break;
         }
         visit[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (visit[v] == false && e[u][v] != inf) {
+                if (dis[u] + e[u][v] < dis[v]) {
+                    dis[v] = dis[u] + e[u][v];
+                    num[v] = num[u];
+                    w[v] = w[u] + weight[v];
+                } else if (dis[u] + e[u][v] == dis[v]) {
+                    num[v] = num[v] + num[u];
+                    if (w[u] + weight[v] > w[v])
+                        w[v] = w[v] + weight[v];
+                }
+            }
+        }
     }
-    
+    printf("%d %d", num[c2], w[c2]);
     return 0;
 }
