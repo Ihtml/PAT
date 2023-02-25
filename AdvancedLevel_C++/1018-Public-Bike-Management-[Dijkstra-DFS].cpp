21673d2806065884e5de395ff68309ab871de4cc
Ihtml <2483690020@qq.com>
2023-02-25 11:02:20 +0800

    feat(pat-a-1018): temporarily commit

--- AdvancedLevel_C++/1018-Public-Bike-Management-[Dijkstra-DFS].cpp
+++ AdvancedLevel_C++/1018-Public-Bike-Management-[Dijkstra-DFS].cpp
@@ -8,6 +8,40 @@ int minNeed = inf, minBack = inf;
 int e[510][510], dis[510], weight[510];
 bool visit[510];
 vector<int> pre[510];
+vector<int> path, temppath;
+void dfs(int v) {
+    temppath.push_back(v);
+    if (v == 0) {
+        int need = 0, back = 0;
+        for (int i = temppath.size() - 1; i >= 0; i--) {
+            int id = temppath[i];
+            if (weight[id] > 0) {
+                back += weight[id];
+            } else {
+                if (back > (0 - weight[id])) {
+                    back += weight[id];
+                } else {
+                    need += ((0 - weight[id]) - back);
+                    back = 0;
+                }
+            }
+        }
+        if (need < minNeed) {
+            minNeed = need;
+            minBack = back;
+            path = temppath;
+        } else if (need == minNeed && back < minBack) {
+            minBack = back;
+            path = temppath;
+        }
+        temppath.pop_back();
+        return;
+    }
+    for (int i = 0; i < pre[v].size(); i++) {
+        dfs(pre[v][i]);
+    }
+    temppath.pop_back();
+}
 int main() {
     fill(e[0], e[0] + 510 * 510, inf);
     fill(dis, dis + 510, inf);
@@ -20,7 +54,7 @@ int main() {
         int a, b;
         scanf("%d%d", &a, &b);
         scanf("%d", &e[a][b]);
-        e[a][b] = e[a][b];
+        e[b][a] = e[a][b];
     }
     dis[0] = 0;
     for (int i = 0; i <= n; i++) {
@@ -46,6 +80,11 @@ int main() {
             }
         }
     }
-    
+    dfs(sp);
+    printf("%d 0", minNeed);
+    for (int i = path.size() - 2; i >= 0; i--) {
+        printf("->%d", path[i]);
+    }
+    printf(" %d", minBack);
     return 0;
 }
