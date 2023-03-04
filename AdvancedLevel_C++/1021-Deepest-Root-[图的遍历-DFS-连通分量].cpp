ac0a5f3c26fdf3b0f727e327dc771a019f55b4b5
Ihtml <2483690020@qq.com>
2023-03-04 10:51:23 +0800

    feat(pat-a-1021): dfs

--- AdvancedLevel_C++/1021-Deepest-Root-[图的遍历-DFS-连通分量].cpp
+++ AdvancedLevel_C++/1021-Deepest-Root-[图的遍历-DFS-连通分量].cpp
@@ -3,7 +3,23 @@
 using namespace std;
 int n, maxheight = 0;
 vector<vector<int>> v;
+bool visit[100];
 vector<int> temp;
+void dfs(int node, int height) {
+    if (height > maxheight) {
+        temp.clear();
+        temp.push_back(node);
+        maxheight = height;
+    } else if (height == maxheight) {
+        temp.push_back(node);
+    }
+    visit[node] = true;
+    for (int i = 0; i < v[node].size(); i++) {
+        if (visit[v[node][i]] == false) {
+            dfs(v[node][i], height + 1);
+        }
+    }
+}
 int main() {
     scanf("%d", &n);
     v.resize(n + 1);
