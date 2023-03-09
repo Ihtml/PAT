66a0929262c69f4ae906296148de6058e0c42626
Ihtml <2483690020@qq.com>
2023-03-09 10:05:56 +0800

    feat(pat-a-1022): ac

--- AdvancedLevel_C++/1022-Digital-Library-[map映射]-30.cpp
+++ AdvancedLevel_C++/1022-Digital-Library-[map映射]-30.cpp
@@ -3,6 +3,15 @@
 #include <set>
 using namespace std;
 map<string, set<int> > title, author, key, pub, year;
+void query(map<string, set<int> >& m, string& str) {
+    if (m.find(str) != m.end()) {
+        for (auto it = m[str].begin(); it != m[str].end(); it++) {
+            printf("%07d\n", *it);
+        }
+    } else {
+        cout << "Not Found\n";
+    }
+}
 int main() {
     int n, m, id, num;
     scanf("%d", &n);
@@ -23,6 +32,24 @@ int main() {
         pub[tpub].insert(id);
         getline(cin, tyear);
         year[tyear].insert(id);
+    }
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d: ", &num);
+        string temp;
+        getline(cin, temp);
+        cout << num << ": " << temp << "\n";
+        if (num == 1) {
+            query(title, temp);
+        } else if (num == 2) {
+            query(author, temp);
+        } else if (num == 3) {
+            query(key, temp);
+        } else if (num == 4) {
+            query(pub, temp);
+        } else if (num == 5) {
+            query(year, temp);
+        }
     }
     return 0;
 }
