40351d210412bc34ebd2f5cf07e3379d30f1d0bf
Ihtml <2483690020@qq.com>
2023-03-19 21:54:36 +0800

    feat(pat-a-1029): init commit

---
+++ AdvancedLevel_C++/1029-Median-[双指针].cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int k[200005];
+int main() {
+    int n, m, temp, count = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &k[i]);
+    }
+
+    return 0;
+}
