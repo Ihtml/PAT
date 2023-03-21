e5bdb4b50193dbc0473714660d323c391decb2af
Ihtml <2483690020@qq.com>
2023-03-21 09:39:23 +0800

    feat(pat-a-1029): ac

--- AdvancedLevel_C++/1029-Median-[双指针].cpp
+++ AdvancedLevel_C++/1029-Median-[双指针].cpp
@@ -8,29 +8,31 @@ int main() {
     for (int i = 1; i <= n; i++) {
         scanf("%d", &k[i]);
     }
-    k[n+1] = INT_MAX;
+    k[n + 1] = INT_MAX;  // 如果不设k[n+1]为最大值，则k[n+1]及以后都为0，比较时会出错
     cin >> m;
     int midpos = (n + m + 1) / 2, i = 1;
-    for (int j = 1; j <= m; j++) {
+    for (int j = 1; i<=n && j <= m; j++) {
         scanf("%d", &temp);
-        while (k[i] < temp) {
+        while (k[i] <= temp) {
             count++;
             if (count == midpos) {
                 cout << k[i];
+                return 0;
             }
             i++;
         }
         count++;
         if (count == midpos) {
-            count << temp;
+            cout << temp;
+            return 0;
         }
     }
-    while (i <= n) {
+    while (count < midpos) {
         count++;
         if (count == midpos) {
-            count << k[i];
+            cout << k[i];
+            return 0;
         }
         i++;
     }
-    return 0;
 }
