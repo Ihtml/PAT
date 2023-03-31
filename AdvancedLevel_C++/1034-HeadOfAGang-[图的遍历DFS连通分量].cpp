5ae40d5b9bcc0b7aded7190148c8525507691af6
Ihtml <2483690020@qq.com>
2023-03-31 09:38:56 +0800

    feat(pat-a-1034): add dfs

--- AdvancedLevel_C++/1034-HeadOfAGang-[图的遍历DFS连通分量].cpp
+++ AdvancedLevel_C++/1034-HeadOfAGang-[图的遍历DFS连通分量].cpp
@@ -16,6 +16,34 @@ int stoifunc(string s) {
 }
 const int N = 1010;
 int G[N][N], weight[N];
+bool vis[2010];
+void dfs(int u, int& head, int& numMember, int& totalweight) {
+    vis[u] = true;
+    numMember++;
+    if (weight[u] > weight[head]) {
+        head = u;
+    }
+    for (int v = 1; v < idNum; v++) {
+        if (G[u][v] > 0) {
+            totalweight += G[u][v];
+            G[u][v] = G[v][u] = 0;
+            if (vis[v] == false) {
+                dfs(v, head, numMember, totalweight);
+            }
+        }
+    }
+}
+void dfsTrave() {
+    for (int i = 1; i < idNum; i++) {
+        if (vis[i] == false) {
+            int head = i, numMember = 0, totalweight = 0;
+            dfs(i, head, numMember, totalweight);
+            if (numMember > 2 && totalweight > k) {
+                ans[intToString[head]] = numMember;
+            }
+        }
+    }
+}
 int main() {
     int n, w;
     scanf("%d%d", &n, &k);
@@ -29,6 +57,7 @@ int main() {
         G[id1][id2] += w;
         G[id2][id1] += w;
     }
+    dfsTrave();
 
     return 0;
 }
