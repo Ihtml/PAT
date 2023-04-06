d8cee8c7a105be71064ca2634ed5b04f0141d186
Ihtml <2483690020@qq.com>
2023-04-06 10:48:30 +0800

    feat(pat-a-1043): init commit

---
+++ AdvancedLevel_C++/1043-IsItABinarySearchTree-[二叉查找树BST].cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> pre, post;
+bool isMirror;
+int main() {
+    int n;
+    cin >> n;
+    pre.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> pre[i];
+    }
+
+    return 0;
+}
