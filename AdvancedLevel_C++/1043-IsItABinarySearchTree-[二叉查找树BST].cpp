49f1f9002bb527cf1f5eb859636d88527235c254
Ihtml <2483690020@qq.com>
2023-04-08 11:18:42 +0800

    feat(pat-a-1043): getpost

--- AdvancedLevel_C++/1043-IsItABinarySearchTree-[二叉查找树BST].cpp
+++ AdvancedLevel_C++/1043-IsItABinarySearchTree-[二叉查找树BST].cpp
@@ -3,6 +3,40 @@
 using namespace std;
 vector<int> pre, post;
 bool isMirror;
+/*
+    8
+  6   10
+5  7 8   11
+pre: 8 6 5 7 10 8 11
+post: 5 7 6 8 11 10 8
+*/
+void getpost(int root, int tail) {
+    if (root > tail) {
+        return;
+    }
+    int i = root + 1, j = tail;
+    if (!isMirror) {
+        while (i <= tail && pre[root] > pre[i]) {
+            i++;
+        }
+        while (j > root && pre[root] <= pre[j]) {
+            j--;
+        }
+    } else {
+        while (i <= tail && pre[root] <= pre[i]) {
+            i++;
+        }
+        while (j > root && pre[root] > pre[j]) {
+            j--;
+        }
+    }
+    if (i - j != 1) {
+        return;
+    }
+    getpost(root + 1, j);  // 遍历左子树
+    getpost(i, tail);      // 遍历右子树
+    post.push_back(pre[root]);
+}
 int main() {
     int n;
     cin >> n;
