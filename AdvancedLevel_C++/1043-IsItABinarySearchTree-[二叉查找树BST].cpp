5bfea1249ba85af8a1c7eced417973289963ad45
Ihtml <2483690020@qq.com>
2023-04-09 11:52:13 +0800

    feat(pat-a-1043): ac

--- AdvancedLevel_C++/1043-IsItABinarySearchTree-[二叉查找树BST].cpp
+++ AdvancedLevel_C++/1043-IsItABinarySearchTree-[二叉查找树BST].cpp
@@ -17,10 +17,10 @@ void getpost(int root, int tail) {
     int i = root + 1, j = tail;
     if (!isMirror) {
         while (i <= tail && pre[root] > pre[i]) {
-            i++;
+            i++;  // 根结点右子树的节点
         }
         while (j > root && pre[root] <= pre[j]) {
-            j--;
+            j--;  // 根结点左子树的结点
         }
     } else {
         while (i <= tail && pre[root] <= pre[i]) {
@@ -44,6 +44,19 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> pre[i];
     }
-
+    getpost(0, n - 1);
+    if (post.size() != n) {
+        isMirror = true;  // 假设为镜像
+        post.clear();
+        getpost(0, n - 1);
+    }
+    if (post.size() == n) {
+        printf("YES\n%d", post[0]);
+        for (int i = 1; i < n; i++) {
+            printf(" %d", post[i]);
+        }
+    } else {
+        printf("NO");
+    }
     return 0;
 }
