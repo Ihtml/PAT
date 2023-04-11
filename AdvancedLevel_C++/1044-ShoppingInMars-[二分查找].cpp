f97a53d342f469ce673a08b3c33cfae666f95cb4
Ihtml <2483690020@qq.com>
2023-04-11 10:03:16 +0800

    feat(pat-a-1044): temporarily commit

--- AdvancedLevel_C++/1044-ShoppingInMars-[二分查找].cpp
+++ AdvancedLevel_C++/1044-ShoppingInMars-[二分查找].cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+Q:求⼀串的数字中连续的⼀段，使得这个连续的段内数字的和恰好等于所期望的值m。如果
+不能找到恰好等于，就找让⾃自己付出最少的价格(总和必须⼤于等于所给值)的那段区间。求所有可
+能的结果。
+*/
 vector<int> sum, resultArr;
 int n, m;
+void Func(int i, int& j, int& tempsum) {  // 二分查找
+    int left = i, right = n;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (sum[mid] - sum[i - 1] >= m) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    j = right;
+    tempsum = sum[j] - sum[i - 1];
+}
 int main() {
     cin >> n >> m;
     sum.resize(n + 1);
@@ -11,4 +29,12 @@ int main() {
         sum[i] += sum[i - 1];
     }
     int minans = sum[n];
+    for (int i = 1; i <= n; i++) {
+        int j, tempsum;
+        Func(i, j, tempsum);
+        if (tempsum > minans) {
+            continue;
+        }
+    }
+    return 0;
 }
