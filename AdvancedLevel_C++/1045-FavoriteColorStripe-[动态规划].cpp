55941cb124211e5d699d78d8cf6f99a91395b262
Ihtml <2483690020@qq.com>
2023-04-14 10:01:36 +0800

    feat(pat-a-1045): ac

--- AdvancedLevel_C++/1045-FavoriteColorStripe-[动态规划].cpp
+++ AdvancedLevel_C++/1045-FavoriteColorStripe-[动态规划].cpp
@@ -7,18 +7,27 @@ using namespace std;
 int book[201], a[10001], dp[10001];
 int main() {
     int n, m, l, x, num = 0, maxn = 0;
-    cin >> n >> m;
+    cin >> n >> m;  // 共n种颜色 m个喜欢的颜色
     for (int i = 1; i <= m; i++) {
         cin >> x;
         book[x] = i;
     }
     cin >> l;
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < l; i++) {
         cin >> x;
-        if (book[x] >= 1) {
+        if (book[x] >= 1) {  // 是喜欢的颜色
             a[num++] = book[x];
         }
     }
-
+    for (int i = 0; i < num; i++) {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (a[i] >= a[j]) {  // 比较是否符合顺序
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        maxn = max(dp[i], maxn);
+    }
+    cout << maxn;
     return 0;
 }
