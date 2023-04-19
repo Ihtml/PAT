b01d4010439b85d8fb502c0b6ada79b6246d0123
Ihtml <2483690020@qq.com>
2023-04-19 11:41:30 +0800

    feat(pat-a-1049): temporary commit

--- AdvancedLevel_C++/1049-CountingOnes-[数学问题].cpp
+++ AdvancedLevel_C++/1049-CountingOnes-[数学问题].cpp
@@ -1,9 +1,24 @@
+/*
+Q:给出一个数字n，求1~n的所有数字⾥⾯面出现1的个数
+
+*/
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
+int main() {
+    // now为当前位的数字，left为now左边的所有数字构成的数字，right是now右边的所有数字构成的数字
+    int n, left = 0, right = 0, a = 1, now = 1, ans = 0;
     cin >> n;
-
-    
+    while (n / a) {
+        left = n / (a * 10), now = n / a % 10, right = n % a;
+        if (now == 0) {
+            ans += left * a;
+        } else if (now == 1) {
+            ans += left* a + right + 1;
+        } else {
+            ans += (left + 1) * a;
+        }
+        a = a * 10;
+    }
+    cout << ans;
     return 0;
 }
