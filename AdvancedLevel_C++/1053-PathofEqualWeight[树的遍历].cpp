a0525b2661cf80e047894e0a6f049571f107a920
Ihtml <2483690020@qq.com>
2023-04-24 10:06:58 +0800

    feat(pat-a-1053): ac

--- AdvancedLevel_C++/1053-PathofEqualWeight[树的遍历].cpp
+++ AdvancedLevel_C++/1053-PathofEqualWeight[树的遍历].cpp
@@ -12,6 +12,29 @@ struct NODE {
 vector<NODE> v;
 vector<int> path;
 int target;
+void dfs(int index, int nodeNum, int sum) {
+    if (sum > target) {
+        return;
+    }
+    if (sum == target) {
+        if (v[index].child.size() != 0) {  // 没有到达叶结点
+            return;
+        }
+        for (int i = 0; i < nodeNum; i++) {  // 有多个叶结点
+            printf("%d%c", v[path[i]].w, i != nodeNum - 1 ? ' ' : '\n');
+            // 根结点是path[0]
+        }
+        return;
+    }
+    for (int i = 0; i < v[index].child.size(); i++) {
+        int node = v[index].child[i];
+        path[nodeNum] = node;  // 这里path从1开始
+        dfs(node, nodeNum + 1, sum + v[node].w);
+    }
+}
+int cmp(int a, int b) {
+    return v[a].w > v[b].w;
+}
 int main() {
     int n, m, node, k;
     scanf("%d %d %d", &n, &m, &target);
@@ -26,6 +49,8 @@ int main() {
         for (int j = 0; j < k; j++) {
             scanf("%d", &v[node].child[j]);
         }
+        sort(v[node].child.begin(), v[node].child.end(), cmp);
     }
+    dfs(0, 1, v[0].w);
     return 0;
 }
