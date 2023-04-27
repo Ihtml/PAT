bdb16e04393f832e0d034bf970bed400f5b99532
Ihtml <2483690020@qq.com>
2023-04-27 11:27:20 +0800

    feat(pat-a-1056): ac

--- AdvancedLevel_C++/1056-MiceAndRice-[queue].cpp
+++ AdvancedLevel_C++/1056-MiceAndRice-[queue].cpp
@@ -25,9 +25,48 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &num);
         w[i].weight = v[num];
-        w[i].index = i; 
-        w[i].index0 = num;  // 比赛顺序
+        w[i].index = i;  // 比赛顺序
+        w[i].index0 = num;  // id几号选手
     }
-
+    queue<node> q;
+    for (int i = 0; i < n; i++) {
+        q.push(w[i]);
+    }
+    while (!q.empty()) {
+        int size = q.size();
+        if (size == 1) {  // 只剩下一个 就是第一名
+            node temp = q.front();
+            w[temp.index].rank = 1;
+            break;
+        }
+        int group = size / g;  // 分成几队
+        if (size % g != 0) {
+            group += 1;
+        }
+        node maxnode;
+        int maxn = -1, cnt = 0;
+        for (int i = 0; i < size; i++) {
+            node temp = q.front();
+            w[temp.index].rank = group + 1;
+            q.pop();
+            cnt++;
+            if (temp.weight > maxn) {
+                maxn = temp.weight;
+                maxnode = temp;
+            }
+            if (cnt == g || i == size - 1) {  // 一小组比较结束
+                cnt = 0;
+                maxn = -1;
+                q.push(maxnode);
+            }
+        }
+    }
+    sort(w.begin(), w.end(), cmp);
+    for (int i = 0; i < n; i++) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", w[i].rank);
+    }
     return 0;
 }
