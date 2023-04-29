dfd68c702629819746538269914e9c415ed9d386
Ihtml <2483690020@qq.com>
2023-04-29 10:31:09 +0800

    feat(pat-a-1057): update

--- AdvancedLevel_C++/1057-Stack[树状数组].cpp
+++ AdvancedLevel_C++/1057-Stack[树状数组].cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <stack>
+#define lowbit(i) ((i) & (-i))
+const int maxn = 100010;
 using namespace std;
+int c[maxn];
 stack<int> s;
+void update(int x, int v) {
+    for (int i = x; i < maxn; i += lowbit(i)) {
+        c[i] += v;
+    }
+}
 int main() {
-    int n;
+    int n, temp;
     scanf("%d", &n);
     string str;
     for (int i = 0; i < n; i++) {
         cin >> str;
         switch (str[1]) {
             case 'u':  // push操作
-
+                scanf("%d", temp);
+                s.push(temp);
+                update(temp, 1);
                 break;
 
             case 'o':  // pop操作
                 if (!s.empty()) {
+                    update(s.top(), -1);
+                    printf("%d\n", s.top());
+                    s.top();
                 } else {
                     printf("Invalid\n");
                 }
