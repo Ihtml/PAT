d210a0029956d2a8964e6f9186570f95b7795165
Ihtml <2483690020@qq.com>
2023-04-30 10:23:45 +0800

    feat(pat-a-1057): ac

--- AdvancedLevel_C++/1057-Stack[树状数组].cpp
+++ AdvancedLevel_C++/1057-Stack[树状数组].cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include <stack>
-#define lowbit(i) ((i) & (-i))
-const int maxn = 100010;
+#define lowbit(i) ((i) & (-i))  // 宏定义lowbit(i)
+const int maxn = 100010;        // 数组c的大小
 using namespace std;
-int c[maxn];
-stack<int> s;
+
+int c[maxn];   // 树状数组，存储每个数字出现的次数
+stack<int> s;  // 栈，存储输入的数字
+
+// 更新树状数组
 void update(int x, int v) {
-    for (int i = x; i < maxn; i += lowbit(i)) {
-        c[i] += v;
+    for (int i = x; i < maxn; i += lowbit(i)) {  // 从x开始向后更新
+        c[i] += v;  // 更新数字x在树状数组中的出现次数
     }
+}
+
+// 获取前缀和
+int getsum(int x) {
+    int sum = 0;
+    for (int i = x; i >= 1; i -= lowbit(i)) {  // 从x开始向前获取前缀和
+        sum += c[i];  // 累加数字i在树状数组中的出现次数
+    }
+    return sum;
+}
+
+// 查找中位数
+void PeekMedian() {
+    int left = 1, right = maxn, mid,
+        k = ((s.size() + 1) / 2);  // 初始化left、right、k
+    while (left < right) {         // 二分查找
+        mid = (left + right) / 2;  // 计算中间位置
+        if (getsum(mid) >= k) {  // 中间位置的数字出现次数大于等于k
+            right = mid;         // 向左查找
+        } else {
+            left = mid + 1;  // 向右查找
+        }
+    }
+    printf("%d\n", left);  // 输出中位数
 }
 int main() {
     int n, temp;
@@ -18,7 +45,7 @@ int main() {
         cin >> str;
         switch (str[1]) {
             case 'u':  // push操作
-                scanf("%d", temp);
+                scanf("%d", &temp);
                 s.push(temp);
                 update(temp, 1);
                 break;
@@ -27,19 +54,18 @@ int main() {
                 if (!s.empty()) {
                     update(s.top(), -1);
                     printf("%d\n", s.top());
-                    s.top();
+                    s.pop();
                 } else {
                     printf("Invalid\n");
                 }
-
                 break;
 
             default:  // median操作
                 if (!s.empty()) {
+                    PeekMedian();
                 } else {
                     printf("Invalid\n");
                 }
-
                 break;
         }
     }
