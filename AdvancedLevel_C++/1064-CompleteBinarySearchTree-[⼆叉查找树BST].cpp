f465b1cb8f6ef45f2c02c4e47d34e11dada28a2d
Ihtml <2483690020@qq.com>
2023-05-04 15:58:42 +0800

    feat(pat-a-1064): add notes

--- AdvancedLevel_C++/1064-CompleteBinarySearchTree-[⼆叉查找树BST].cpp
+++ AdvancedLevel_C++/1064-CompleteBinarySearchTree-[⼆叉查找树BST].cpp
@@ -14,7 +14,10 @@ void levelorder(int start, int end, int index) {
     int n = end - start + 1;
     int l = log(n + 1) / log(2);  // ceil(log2(n+1))  除了最后一层的层数
     int leave = n - (pow(2, l) - 1);  // 最后一层的叶子节点数
-    int root = start + (pow(2, l - 1) - 1) + min((int)pow(2, l - 1), leave);  //
+    int root = start + (pow(2, l - 1) - 1) + min((int)pow(2, l - 1), leave);
+    // pow(2, l - 1) - 1是除了root结点所在层和最后一层外，左子树的结点个数
+    //  pow(2, l - 1) 是l+1 层最多拥有的属于根结点左子树的结点个数
+    // min(pow(2, l - 1), leave)是最后⼀个结点真正拥有的属于根结点左子树上的结点个数
     level[index] = in[root];
     levelorder(start, root - 1, 2 * index + 1);
     levelorder(root + 1, end, 2 * index + 2);
@@ -31,7 +34,7 @@ int main() {
     levelorder(0, n - 1, 0);
     printf("%d", level[0]);
     for (int i = 1; i < n; i++) {
-        printf("% d", level[i]);
+        printf(" %d", level[i]);
     }
     return 0;
 }
