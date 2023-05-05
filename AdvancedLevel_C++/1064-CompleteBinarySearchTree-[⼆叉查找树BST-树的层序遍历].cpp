68bcaeee4fb871b270b80a5258b0984ab8492d44
Ihtml <2483690020@qq.com>
2023-05-05 13:09:50 +0800

    feat(pat-a-1064): ac

--- AdvancedLevel_C++/1064-CompleteBinarySearchTree-[⼆叉查找树BST-树的层序遍历].cpp
+++ AdvancedLevel_C++/1064-CompleteBinarySearchTree-[⼆叉查找树BST-树的层序遍历].cpp
@@ -6,6 +6,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int In[1010], lel[1010], n, t = 0;
+void inOrder(int root) {
+    if (root >= n)
+        return;
+    inOrder(root * 2 + 1);
+    lel[root] = In[t++];
+    inOrder(root * 2 + 2);
+}
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &In[i]);
+    }
+    sort(In, In + n);
+    inOrder(0);
+    printf("%d", lel[0]);
+    for (int i = 1; i < n; i++) {
+        printf(" %d", lel[i]);
+    }
+    return 0;
+}
+
 vector<int> in, level;
 void levelorder(int start, int end, int index) {
     if (start > end) {
@@ -17,12 +40,13 @@ void levelorder(int start, int end, int index) {
     int root = start + (pow(2, l - 1) - 1) + min((int)pow(2, l - 1), leave);
     // pow(2, l - 1) - 1是除了root结点所在层和最后一层外，左子树的结点个数
     //  pow(2, l - 1) 是l+1 层最多拥有的属于根结点左子树的结点个数
-    // min(pow(2, l - 1), leave)是最后⼀个结点真正拥有的属于根结点左子树上的结点个数
+    // min(pow(2, l - 1),
+    // leave)是最后⼀个结点真正拥有的属于根结点左子树上的结点个数
     level[index] = in[root];
     levelorder(start, root - 1, 2 * index + 1);
     levelorder(root + 1, end, 2 * index + 2);
 }
-int main() {
+int main0() {
     int n;
     scanf("%d", &n);
     in.resize(n);
