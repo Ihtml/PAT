ea332826315feff52cc64f0cb4a8fec24237ec0a
Ihtml <2483690020@qq.com>
2023-05-20 22:51:27 +0800

    feat(pat-a-1075): update

--- AdvancedLevel_C++/1075-PAT-Judge-[排序].cpp
+++ AdvancedLevel_C++/1075-PAT-Judge-[排序].cpp
@@ -5,8 +5,8 @@ using namespace std;
 struct node {
     int rank, id, total = 0;
     vector<int> score;
-    int passnum = 0;
-    bool isshown = false;
+    int passnum = 0;       // 完整通过的题⽬目个数
+    bool isshown = false;  // 至少有一题通过
 };
 bool cmp(node a, node b) {
     if (a.total != b.total) {
@@ -17,10 +17,27 @@ bool cmp(node a, node b) {
         return a.id < b.id;
     }
 }
-int main(){
+int main() {
     int n, k, m, id, num, score;
     scanf("%d %d %d", &n, &k, &m);
-    vector<node> v(n+1);
-    // todo
+    vector<node> v(n + 1);
+    for (int i = 1; i <= n; i++) {
+        v[i].score.resize(k + 1, -1);
+    }
+    vector<int> full(k + 1);
+    for (int i = 1; i <= k; i++) {
+        scanf("%d", &full[i]);
+    }
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &id, &num, &score);
+        v[id].id = id;
+        v[id].score[num] = max(v[id].score[num], score);
+        if (score != -1) {
+            v[id].isshown = true;
+        } else if (v[id].score[num] == -1) {
+            v[id].score[num] = -2;
+        }
+    }
+
     return 0;
 }
