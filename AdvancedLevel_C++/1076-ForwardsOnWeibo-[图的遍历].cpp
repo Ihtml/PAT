298e15941d4878eab9f0d9b2bfb6983390b1693d
Ihtml <2483690020@qq.com>
2023-05-24 13:38:45 +0800

    feat(pat-a-1076): update

--- AdvancedLevel_C++/1076-ForwardsOnWeibo-[图的遍历].cpp
+++ AdvancedLevel_C++/1076-ForwardsOnWeibo-[图的遍历].cpp
@@ -2,6 +2,14 @@
 #include <vector>
 using namespace std;
 int n, m, l, k;
+struct node {
+    int id, layer;
+};
+int bfs(node tnode) {
+    int cnt = 0;
+    // todo
+    return cnt;
+}
 vector<vector<int> > v;
 int main() {
     scanf("%d %d", &n, &l);
@@ -14,5 +22,13 @@ int main() {
             v[t].push_back(i);
         }
     }
+    scanf("%d", &k);
+    int tid;
+    for (int i = 0; i < k; i++) {
+        scanf("%d", &tid);
+        node tnode = {tid, 0};
+        // printf("%d\n", bfs(tnode));
+    }
+
     return 0;
 }
