afb90e8516b1bd3f3b3d5b1e6cc5152de19edb0d
Ihtml <2483690020@qq.com>
2023-05-27 10:15:23 +0800

    feat(pat-a-1079): update

--- AdvancedLevel_C++/1079-TotalSalesOfSupplyChain-[树的遍历BFSDFS].cpp
+++ AdvancedLevel_C++/1079-TotalSalesOfSupplyChain-[树的遍历BFSDFS].cpp
@@ -1,3 +1,9 @@
+/*
+题目大意：给一棵树，在树根出货物的价格为p，
+从根结点开始每往下走一层，该层的货物价格将会在父亲结点的价格上增加r%，
+给出每个叶结点的货物量，求他们的价格之和
+*/
+#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -12,6 +18,17 @@ int main() {
     scanf("%d %lf %lf", &n, &p, &r);
     r = r / 100;
     v.resize(n);
-    printf("%.1f", ans*p);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &k);
+        if (k == 0) {
+            scanf("%lf", &v[i].data);
+        } else {
+            for (int j = 0; j < k; j++) {
+                scanf("%d", &c);
+                v[i].child.push_back(c);
+            }
+        }
+    }
+    printf("%.1f", ans * p);
     return 0;
 }
