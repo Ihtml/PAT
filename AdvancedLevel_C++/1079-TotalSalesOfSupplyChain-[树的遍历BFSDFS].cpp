3bfbb3255ffa9655d991cb47c6adcf80602ab5cb
Ihtml <2483690020@qq.com>
2023-05-28 09:30:30 +0800

    feat(pat-a-1079): ac

--- AdvancedLevel_C++/1079-TotalSalesOfSupplyChain-[树的遍历BFSDFS].cpp
+++ AdvancedLevel_C++/1079-TotalSalesOfSupplyChain-[树的遍历BFSDFS].cpp
@@ -13,6 +13,15 @@ struct node {
 };
 vector<node> v;
 double ans = 0.0, p, r;
+void dfs(int index, int depth) {
+    if (v[index].child.size() == 0) {
+        ans += v[index].data * pow(1 + r, depth);
+        return;
+    }
+    for (int i = 0; i < v[index].child.size(); i++) {
+        dfs(v[index].child[i], depth + 1);
+    }
+}
 int main() {
     int n, k, c;
     scanf("%d %lf %lf", &n, &p, &r);
@@ -29,6 +38,7 @@ int main() {
             }
         }
     }
+    dfs(0, 0);
     printf("%.1f", ans * p);
     return 0;
 }
