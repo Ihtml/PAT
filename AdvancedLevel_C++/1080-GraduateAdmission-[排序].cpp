7cfea58686c081efc3cb983a6b6309a13c2fc366
Ihtml <2483690020@qq.com>
2023-05-30 10:05:30 +0800

    feat(pat-a-1080): init commit

---
+++ AdvancedLevel_C++/1080-GraduateAdmission-[排序].cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+struct stu {
+    int id, ge, gi, fin;
+    vector<int> choice;
+};
+
+int main() {
+    int n, m, k, quota[105];  // n-申请学生总数 m-学校数量 k-学生志愿数
+    scanf("%d%d%d", &n, &m, &k);
+    int cnt[110] = {0};  // 所有元素初始化为0
+    return 0;
+}
