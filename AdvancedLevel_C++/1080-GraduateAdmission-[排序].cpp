3b2a4538bca3a12b7c80c138e42265c91b80640d
Ihtml <2483690020@qq.com>
2023-05-31 10:30:10 +0800

    feat(pat-a-1080): update

--- AdvancedLevel_C++/1080-GraduateAdmission-[排序].cpp
+++ AdvancedLevel_C++/1080-GraduateAdmission-[排序].cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct stu {
+struct peo {
     int id, ge, gi, fin;
     vector<int> choice;
 };
@@ -11,5 +11,19 @@ int main() {
     int n, m, k, quota[105];  // n-申请学生总数 m-学校数量 k-学生志愿数
     scanf("%d%d%d", &n, &m, &k);
     int cnt[110] = {0};  // 所有元素初始化为0
+    vector<peo> stu(n), sch[105];
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &quota[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d", &stu[i].ge, &stu[i].gi);
+        stu[i].id = i;
+        stu[i].fin = stu[i].ge + stu[i].gi;
+        stu[i].choice.resize(k);
+        for (int j = 0; j < k; j++) {
+            scanf("%d", &stu[i].choice[j]);
+        }
+    }
+
     return 0;
 }
