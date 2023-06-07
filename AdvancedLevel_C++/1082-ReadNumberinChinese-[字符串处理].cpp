dfcc4abed62586e22e671b3767ab9f42efde438d
Ihtml <2483690020@qq.com>
2023-06-07 11:19:50 +0800

    feat(pat-a-1082): ac

--- AdvancedLevel_C++/1082-ReadNumberinChinese-[字符串处理].cpp
+++ AdvancedLevel_C++/1082-ReadNumberinChinese-[字符串处理].cpp
@@ -20,7 +20,7 @@ int main() {
         return 0;
     }
     if (n < 0) {
-        cout << "Fu";
+        cout << "Fu ";
         n = -n;
     }
     int part[3];
@@ -28,9 +28,11 @@ int main() {
     part[1] = (n % 100000000) / 10000;
     part[2] = n % 10000;
     bool zero = false;  // 是否在非零数字前输出合适的ling
-    int printCnt = 0;  // 用于维护单词前没有空格，之后输入的单词都在前面加一个空格。
+    int printCnt =
+        0;  // 用于维护单词前没有空格，之后输入的单词都在前面加一个空格。
     for (int i = 0; i < 3; i++) {
-        int temp = part[i];  // 三个部分，每部分内部的命名规则都一样，都是X千X百X十X
+        int temp =
+            part[i];  // 三个部分，每部分内部的命名规则都一样，都是X千X百X十X
         for (int j = 3; j >= 0; j--) {
             int curPos = 8 - i * 4 + j;  // 当前数字的位置
             if (curPos >= 9) {
@@ -38,9 +40,28 @@ int main() {
             }
             int cur = (temp / J[j]) % 10;  // 取出当前数字
             if (cur != 0) {
+                if (zero) {
+                    printCnt++ == 0 ? cout << "ling" : cout << " ling";
+                    zero = false;
+                }
+                if (j == 0) {
+                    printCnt++ == 0
+                        ? cout << num[cur]
+                        : cout << ' ' << num[cur];  // 在个位，直接输出
+                } else {
+                    printCnt++ == 0 ? cout << num[cur] << ' ' << c[j]
+                                    : cout << ' ' << num[cur] << ' '
+                                           << c[j];  // 在其他位，还要输出十百千
+                }
+            } else {
+                if (!zero && j != 0 && n / J[curPos] >= 10) {
+                    zero = true;  // 注意100020这样的情况
+                }
             }
         }
+        if (i != 2 && part[i] > 0) {
+            cout << ' ' << c[i + 4];  // 处理完每部分之后，最后输出单位，Yi/Wan
+        }
     }
-
     return 0;
 }
