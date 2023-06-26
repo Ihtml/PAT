3f14d3f8ee203dd081d693aee908260704f5286b
Ihtml <2483690020@qq.com>
2023-06-26 22:28:29 +0800

    feat(pat-a-1094): dfs

--- AdvancedLevel_C++/1094-The-Largest-Generation-[树的遍历DFSBFS].cpp
+++ AdvancedLevel_C++/1094-The-Largest-Generation-[树的遍历DFSBFS].cpp
@@ -2,9 +2,16 @@
 #include <vector>
 using namespace std;
 vector<int> v[100];
+int book[100];
+void dfs(int index, int level) {
+    book[level]++;
+    for (int i = 0; i < v[index].size(); i++) {
+        dfs(v[index][i], level + 1);
+    }
+}
 int main() {
     int n, m, a, k, c;
-    scanf("%d %d", &n, &m);
+    scanf("%d %d", &n, &m);  // n-成员总数 m-有孩子的结点的个数
     for (int i = 0; i < m; i++) {
         scanf("%d %d", &a, &k);
         for (int j = 0; j < k; j++) {
@@ -12,6 +19,6 @@ int main() {
             v[a].push_back(c);  // 孩子结点
         }
     }
-    // dfs
+    dfs(1, 1);
     return 0;
 }
