60eb6fc066b41ff573ec9b248af88fa60cb3f8ff
Ihtml <2483690020@qq.com>
2023-06-27 13:44:52 +0800

    feat(pat-a-1094): dfs ac

--- AdvancedLevel_C++/1094-The-Largest-Generation-[树的遍历DFSBFS].cpp
+++ AdvancedLevel_C++/1094-The-Largest-Generation-[树的遍历DFSBFS].cpp
@@ -1,3 +1,6 @@
+/*输入树的结点个数N，结点编号为1~N，非叶子结点个数M，然后输出M个非叶子结点格子的孩子结点的编号，
+求结点个数最多的一层，
+根结点的层号为1，输出该层的结点个数以及层号*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -20,5 +23,13 @@ int main() {
         }
     }
     dfs(1, 1);
+    int maxnum = 0, maxlevel = 1;
+    for (int i = 1; i < 100; i++) {
+        if (book[i] > maxnum) {
+            maxnum = book[i];
+            maxlevel = i;
+        }
+    }
+    printf("%d %d", maxnum, maxlevel);
     return 0;
 }
