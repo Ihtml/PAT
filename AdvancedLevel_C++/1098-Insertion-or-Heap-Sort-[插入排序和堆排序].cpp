84a68d32844961733b72310c688d96d7a47428af
Ihtml <2483690020@qq.com>
2023-07-09 23:21:53 +0800

    feat(pat-a-1098): judge sort

--- AdvancedLevel_C++/1098-Insertion-or-Heap-Sort-[插入排序和堆排序].cpp
+++ AdvancedLevel_C++/1098-Insertion-or-Heap-Sort-[插入排序和堆排序].cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 int main() {
-    int n;
+    int n, p = 2;
     scanf("%d", &n);
     vector<int> a(n + 1), b(n + 1);
     for (int i = 1; i <= n; i++) {
@@ -11,6 +11,18 @@ int main() {
     }
     for (int i = 1; i <= n; i++) {
         scanf("%d", &b[i]);
+    }
+    while (p <= n && b[p - 1] <= b[p]) {
+        p++;
+    }
+    int index = p;  // b[p] < b[p-1]
+    while (p <= n && a[p] == b[p]) {
+        p++;
+    }
+    if (p = n + 1) {
+        printf("Insertion Sort\n");
+    } else {
+        printf("Heap Sort\n");
     }
 
     return 0;
