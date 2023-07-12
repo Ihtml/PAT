3cacbdbf51a1330f75a2d88c9832639c1febc378
Ihtml <2483690020@qq.com>
2023-07-12 23:21:39 +0800

    feat(pat-a-1098): ac

--- AdvancedLevel_C++/1098-Insertion-or-Heap-Sort-[插入排序和堆排序].cpp
+++ AdvancedLevel_C++/1098-Insertion-or-Heap-Sort-[插入排序和堆排序].cpp
@@ -5,7 +5,7 @@
 using namespace std;
 void downAdjust(vector<int>& b, int low, int high) {
     int i = 1, j = i * 2;
-    while (j < high) {
+    while (j <= high) {
         if (j + 1 <= high && b[j] < b[j + 1]) {
             j = j + 1;
         }
@@ -34,7 +34,7 @@ int main() {
     while (p <= n && a[p] == b[p]) {
         p++;
     }
-    if (p = n + 1) {
+    if (p == n + 1) {
         printf("Insertion Sort\n");
         // 插入排序的下一步就是把第一个不符合从小到大的顺序的那个元素插入到前面已排序的里面的合适的位置
         // 那么只要对前几个已排序的+后面一位这个序列sort排序即可
@@ -47,7 +47,11 @@ int main() {
         }
         swap(b[1], b[p]);
         // 调整堆
+        downAdjust(b, 1, p - 1);
     }
-
+    printf("%d", b[1]);
+    for (int i = 2; i <= n; i++) {
+        printf(" %d", b[i]);
+    }
     return 0;
 }
