ae5f6642c97f2be8b11b1df3c0d79130e1f9d9f2
Ihtml <2483690020@qq.com>
2023-07-20 23:06:21 +0800

    feat(pat-a-1102): dfs

--- AdvancedLevel_C++/1102-Invert-a-Binary-Tree-[树的遍历].cpp
+++ AdvancedLevel_C++/1102-Invert-a-Binary-Tree-[树的遍历].cpp
@@ -6,6 +6,16 @@ using namespace std;
 struct node {
     int id, l, r, index, level;  // 每个结点的id, 左右结点,下标值，和当前层数
 } a[100];
+vector<node> v1;
+void dfs(int root, int index, int level) {
+    if (a[root].r != -1) {
+        dfs(a[root].r, index * 2 + 2, level + 1);
+    }
+    v1.push_back({root, 0, 0, index, level});
+    if (a[root].l != -1) {
+        dfs(a[root].l, index * 2 + 1, level + 1);
+    }
+}
 int main() {
     int n, root = 0, have[100];
     cin >> n;
@@ -26,6 +36,9 @@ int main() {
             a[i].r = -1;
         }
     }
-    
+    while (have[root] == 1) {
+        root++;
+    }
+    dfs(root, 0, 0);
     return 0;
 }
