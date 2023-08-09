47264c636537a94ce256f71363b00164e1c445a4
Ihtml <2483690020@qq.com>
2023-08-09 23:11:19 +0800

    feat(pat-a-1110): init commit

---
+++ AdvancedLevel_C++/1111-Online-Map-[Dijkstra+DFS].cpp
@@ -0,0 +1,15 @@
+/*
+Q:给一张地图，两个结点中既有距离也有时间，有的单行有的双向，要求根据地图推荐两条路线：一条是最快到达路线，一条是最短距离的路线。
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+const int inf = 999999999;
+int dis[510], Time[510], e[510][510], w[510][510];
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a, b, flag = 1, len, t;
+    return 0;
+}
