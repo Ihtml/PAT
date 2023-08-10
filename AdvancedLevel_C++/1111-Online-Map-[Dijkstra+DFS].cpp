f1b1e2b5a7977bf506b1526b055d35ab55ca26f7
Ihtml <2483690020@qq.com>
2023-08-10 22:37:12 +0800

    feat(pat-a-1110): init input

--- AdvancedLevel_C++/1111-Online-Map-[Dijkstra+DFS].cpp
+++ AdvancedLevel_C++/1111-Online-Map-[Dijkstra+DFS].cpp
@@ -1,15 +1,33 @@
 /*
 Q:给一张地图，两个结点中既有距离也有时间，有的单行有的双向，要求根据地图推荐两条路线：一条是最快到达路线，一条是最短距离的路线。
 */
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 using namespace std;
 const int inf = 999999999;
-int dis[510], Time[510], e[510][510], w[510][510];
-int main(){
+int dis[510], Time[510], e[510][510], w[510][510], weight[510];
+bool visit[510];
+vector<int> dispath, Timepath, temppath;
+int st, fin, minnode = inf;
+int main() {
+    fill(dis, dis + 510, inf);
+    fill(Time, Time + 510, inf);
+    fill(weight, weight + 510, inf);
+    fill(e[0], e[0] + 510 * 510, inf);
+    fill(w[0], w[0] + 510 * 510, inf);
     int n, m;
     scanf("%d %d", &n, &m);
     int a, b, flag = 1, len, t;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d %d %d", &a, &b, &flag, &len, &t);
+        e[a][b] = len;
+        w[a][b] = t;
+        if (flag != 1) {
+            e[b][a] = len;
+            w[b][a] = t;
+        }
+    }
+
     return 0;
 }
