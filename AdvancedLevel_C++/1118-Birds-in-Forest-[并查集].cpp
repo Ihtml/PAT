5fbcd16f63d674d0c191ec2fa7ff7354f1d372b3
Ihtml <2483690020@qq.com>
2023-08-30 13:17:18 +0800

    feat(pat-a-1118): union

--- AdvancedLevel_C++/1118-Birds-in-Forest-[并查集].cpp
+++ AdvancedLevel_C++/1118-Birds-in-Forest-[并查集].cpp
@@ -5,6 +5,25 @@ int n, m, k;
 const int maxn = 10010;
 int fa[maxn] = {0}, cnt[maxn] = {0};
 bool exist[maxn];
+int findFather(int x) {
+    int a = x;
+    while (x != fa[x]) {
+        x = fa[x];
+    }
+    while (a != fa[a]) {  // 路径压缩
+        int z = a;
+        a = fa[a];
+        fa[z] = x;
+    }
+    return x;
+}
+void Union(int a, int b) {
+    int faA = findFather(a);
+    int faB = findFather(b);
+    if (faA != faB) {
+        fa[faA] = faB;
+    }
+}
 int main() {
     cin >> n;
     for (int i = 0; i <= maxn; i++) {
@@ -14,6 +33,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> k >> id;
         exist[id] = true;
+        for (int j = 0; j < k - 1; j++) {
+            cin >> temp;
+            Union(id, temp);
+            exist[temp] = true;
+        }
     }
 
     return 0;
