454f76b7c1059869bb4abda23cdeab2f8638dba5
Ihtml <2483690020@qq.com>
2023-09-02 08:41:03 +0800

    feat(pat-a-1119): init commit

---
+++ AdvancedLevel_C++/1119-Pre-and-Post-order-Traversals-[前序后序转中序].cpp
@@ -0,0 +1,13 @@
+/*给出一棵树的结点个数n，以及它的前序遍历和后序遍历，输出它的中序遍历，
+如果中序遍历不唯一就输出No，且输出其中一个中序即可，如果中序遍历唯一就输出Yes，并输出它的中序*/
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> in, pre, post;
+bool unique = true;
+int main(){
+    int n;
+    cin >> n;
+    pre.resize(n), post.resize(n);
+    return 0;
+}
