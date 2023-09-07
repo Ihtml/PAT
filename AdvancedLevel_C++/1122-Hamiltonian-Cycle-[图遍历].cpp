57def9bd1367b751b53ae4a58d6d4e176a4a3976
Ihtml <2483690020@qq.com>
2023-09-07 13:24:25 +0800

    feat(pat-a-1122): update

--- AdvancedLevel_C++/1122-Hamiltonian-Cycle-[图遍历].cpp
+++ AdvancedLevel_C++/1122-Hamiltonian-Cycle-[图遍历].cpp
@@ -11,5 +11,17 @@ int main() {
         cin >> t1 >> t2;
         a[t1][t2] = a[t2][t1] = 1;
     }
+    cin >> cnt;
+    while (cnt--) {
+        cin >> k;
+        vector<int> v(k);
+        set<int> s;
+        int flag1 = 1, flag2 = 1;
+        for (int i = 0; i < k; i++) {
+            cin >> v[i];
+            s.insert(v[i]);
+        }
+    }
+
     return 0;
 }
