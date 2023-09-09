0b0a7df366e263c287d18e008697f3bc03270bb0
Ihtml <2483690020@qq.com>
2023-09-09 22:54:39 +0800

    feat(pat-a-1123): init commit

---
+++ AdvancedLevel_C++/1123-Is-It-a-Complete-AVL-Tree-[AVL树].cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+struct node {
+    int val;
+    struct node *left, *right;
+};
+
+int main() {
+    int n, temp;
+    cin >> n;
+    node* tree = NULL;
+    return 0;
+}
