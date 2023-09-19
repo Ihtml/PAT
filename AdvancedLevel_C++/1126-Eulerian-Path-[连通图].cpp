5973dfea9fbdba6dda5f44e76e32aeb348e26fd4
Ihtml <2483690020@qq.com>
2023-09-19 13:22:23 +0800

    feat(pat-a-1126): update

--- AdvancedLevel_C++/1126-Eulerian-Path-[连通图].cpp
+++ AdvancedLevel_C++/1126-Eulerian-Path-[连通图].cpp
@@ -8,9 +8,24 @@ vector<vector<int>> v;
 vector<bool> visit;
 int cnt = 0;
 int main() {
-    int n, m;
+    int n, m, a, b, even = 0;
     cin >> n >> m;
     v.resize(n + 1);
     visit.resize(n + 1);
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[a].push_back(a);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i != 1) {
+            cout << " ";
+        }
+        cout << v[i].size();
+        if (v[i].size() % 2 == 0) {
+            even++;
+        }
+    }
+
     return 0;
 }
