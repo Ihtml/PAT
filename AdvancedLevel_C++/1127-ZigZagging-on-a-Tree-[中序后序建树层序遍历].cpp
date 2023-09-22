1843085aca6cb4f155735e0dd2c25c020bba1bb1
Ihtml <2483690020@qq.com>
2023-09-22 13:13:46 +0800

    feat(pat-a-1127): init commit

---
+++ AdvancedLevel_C++/1127-ZigZagging-on-a-Tree-[中序后序建树层序遍历].cpp
@@ -0,0 +1,10 @@
+/*给出一个树的中序和后序遍历结果，求它的Z字型层序遍历，也就是偶数层从右往左，奇数层从左往右遍历*/
+#include <iostream>
+#include <queue>
+using namespace std;
+int n;
+vector<int> in, post, result[35];
+int main() {
+    cin >> n;
+    return 0;
+}
