7954bd4d4e7ecd62afae2b202f222832dae75cfd
Ihtml <2483690020@qq.com>
2023-09-26 11:33:19 +0800

    feat(pat-a-1127): update

--- AdvancedLevel_C++/1127-ZigZagging-on-a-Tree-[中序后序建树层序遍历].cpp
+++ AdvancedLevel_C++/1127-ZigZagging-on-a-Tree-[中序后序建树层序遍历].cpp
@@ -27,32 +27,42 @@ void dfs(int& index, int inLeft, int inRight, int postLeft, int postRight) {
 // 层序遍历树，按层存储结果
 void bfs() {
     queue<node> q;
-    q.push(node{root, 0}); // 根节点入队，初始深度为0
+    node a;
+    a.index = root;
+    a.depth = 0;
+    q.push(a);  // 根节点入队，初始深度为0
 
     while (!q.empty()) {
         node temp = q.front();
         q.pop();
-        result[temp.depth].push_back(post[temp.index]); // 将当前节点的值存入对应深度的结果
+        result[temp.depth].push_back(
+            post[temp.index]);  // 将当前节点的值存入对应深度的结果
 
         // 左子树入队
+        node b;
+        b.index = tree[temp.index][0];
+        b.depth = temp.depth + 1;
         if (tree[temp.index][0] != 0)
-            q.push(node{tree[temp.index][0], temp.depth + 1});
-
+            q.push(b);
         // 右子树入队
-        if (tree[temp.index][1] != 0)
-            q.push(node{tree[temp.index][1], temp.depth + 1});
+        if (tree[temp.index][1] != 0){
+            b.index = tree[temp.index][1];
+            q.push(b);
+        }
     }
 }
 
 int main() {
     cin >> n;
     in.resize(n + 1), post.resize(n + 1);
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> in[i];
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> post[i];
     }
-
+    dfs(root, 1, n, 1, n);
+    bfs();
+    printf("%d", result[0][0]);
     return 0;
 }
