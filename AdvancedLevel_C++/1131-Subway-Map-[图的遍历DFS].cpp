401e7ae7c64d4d222ae87495686fc0ce89ff140c
Ihtml <2483690020@qq.com>
2023-10-02 23:51:08 +0800

    feat(pat-a-1131): init

---
+++ AdvancedLevel_C++/1131-Subway-Map-[图的遍历DFS].cpp
@@ -0,0 +1,16 @@
+/*找出一条路线，使得对任何给定的起点和终点，可以找出中途经停站最少的路线；如果经停站一样多，则取需要换乘线路次数最少的路线*/
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+int main() {
+    int n, m, k, pre, temp;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> n >> pre;
+        for (int j = 1; j < m; j++) {
+            cin >> temp;
+        }
+    }
+    return 0;
+}
