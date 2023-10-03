7d1c1923b0f2fcbf7b13c1edb4ee2d95fd0f67f2
Ihtml <2483690020@qq.com>
2023-10-03 08:40:06 +0800

    feat(pat-a-1131): update

--- AdvancedLevel_C++/1131-Subway-Map-[图的遍历DFS].cpp
+++ AdvancedLevel_C++/1131-Subway-Map-[图的遍历DFS].cpp
@@ -3,6 +3,9 @@
 #include <unordered_map>
 #include <vector>
 using namespace std;
+vector<vector<int>> v(10000);
+vector<int> path, tempPath;
+int visit[10000], minCnt, minTransfer, start, end1;
 int main() {
     int n, m, k, pre, temp;
     cin >> n;
@@ -10,7 +13,16 @@ int main() {
         cin >> n >> pre;
         for (int j = 1; j < m; j++) {
             cin >> temp;
+            v[pre].push_back(temp);
+            v[temp].push_back(pre);
         }
     }
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> start >> end1;
+        minCnt = 9999, minTransfer = 9999;
+        tempPath.clear();
+    }
+
     return 0;
 }
