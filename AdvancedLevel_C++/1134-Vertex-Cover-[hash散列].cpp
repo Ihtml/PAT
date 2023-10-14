0b3621802b504fd90e750789558567b909b05dd3
Ihtml <2483690020@qq.com>
2023-10-14 23:22:20 +0800

    feat(pat-a-1134): add notes

--- AdvancedLevel_C++/1134-Vertex-Cover-[hash散列].cpp
+++ AdvancedLevel_C++/1134-Vertex-Cover-[hash散列].cpp
@@ -9,33 +9,55 @@ A:对于每一个集合做判断，遍历集合中的每一个元素，
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int main() {
     int n, m, a, b, k, nv, num;
+    
+    // 读取输入中的节点数 n 和边数 m
     cin >> n >> m;
+    
+    // 创建一个名为 v 的 vector 数组，每个元素是一个 vector，用于表示图的邻接列表
     vector<int> v[n];
+    
+    // 读取边的信息，并将其存储在邻接列表中
     for (int i = 0; i < m; i++) {
         cin >> a >> b;
         v[a].push_back(i);
         v[b].push_back(i);
     }
+    
+    // 读取查询次数 k
     cin >> k;
+    
+    // 处理每次查询
     for (int i = 0; i < k; i++) {
-        cin >> nv;
-        int flag = 0;
+        cin >> nv; // 读取当前查询中涉及的节点数
+        
+        int flag = 0; // 用于标记查询结果的标志
+        
+        // 创建一个 hash 数组，用于记录哪些边已经被覆盖
         vector<int> hash(m, 0);
+        
+        // 读取当前查询中的节点信息
         for (int j = 0; j < nv; j++) {
-            cin >> num;
+            cin >> num; // 读取一个节点
+            
+            // 遍历节点的边，将对应边在 hash 数组中标记为已覆盖
             for (int k = 0; k < v[num].size(); k++) {
                 hash[v[num][k]] = 1;
             }
         }
+        
+        // 检查是否所有的边都已经被覆盖
         for (int j = 0; j < m; j++) {
             if (hash[j] == 0) {
-                printf("No\n");
+                printf("No\n"); // 存在未覆盖的边，输出 "No"
                 flag = 1;
                 break;
             }
         }
+        
+        // 如果所有边都被覆盖，输出 "Yes"
         if (flag == 0)
             printf("Yes\n");
     }
