8ec6be4e459d708d57685d5d054cee81c5930938
Ihtml <2483690020@qq.com>
2023-10-16 00:08:52 +0800

    feat(pat-a-1135): struct node

--- AdvancedLevel_C++/1135-Is-It-A-Red-Black-Tree-[红黑树判断].cpp
+++ AdvancedLevel_C++/1135-Is-It-A-Red-Black-Tree-[红黑树判断].cpp
@@ -3,12 +3,21 @@
 #include <vector>
 using namespace std;
 vector<int> arr;
+struct node {
+    int val;
+    struct node *left, *right;
+};
+
 int main() {
     int k, n;
     scanf("%d", &k);
     for (int i = 0; i < k; i++) {
         scanf("%d", &n);
         arr.resize(n);
+        node* root = NULL;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[j]);
+        }
     }
     return 0;
 }
