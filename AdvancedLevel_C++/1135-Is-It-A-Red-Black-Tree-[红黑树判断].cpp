ffdbb5ccf2cfa2183490740dc3007d16c12f3ecf
Ihtml <2483690020@qq.com>
2023-10-22 23:30:35 +0800

    docs(pat-a-1135): add notes

--- AdvancedLevel_C++/1135-Is-It-A-Red-Black-Tree-[红黑树判断].cpp
+++ AdvancedLevel_C++/1135-Is-It-A-Red-Black-Tree-[红黑树判断].cpp
@@ -3,10 +3,14 @@
 #include <vector>
 using namespace std;
 vector<int> arr;
+
+// 定义二叉树结构
 struct node {
     int val;
     struct node *left, *right;
 };
+
+// 递归构建二叉搜索树
 node* build(node* root, int v) {
     if (root == NULL) {
         root = new node();
@@ -19,6 +23,8 @@ node* build(node* root, int v) {
     }
     return root;
 }
+
+// 判断是否满足红黑树性质1：每个节点是红色或黑色
 bool judge1(node* root) {
     if (root == NULL)
         return true;
@@ -30,6 +36,8 @@ bool judge1(node* root) {
     }
     return judge1(root->left) && judge1(root->right);
 }
+
+// 获取以当前节点为根的子树的黑色节点数目
 int getNum(node* root) {
     if (root == NULL)
         return 0;
@@ -37,24 +45,34 @@ int getNum(node* root) {
     int r = getNum(root->right);
     return root->val > 0 ? max(l, r) + 1 : max(l, r);
 }
-bool judge2(node *root) {
-    if (root == NULL) return true;
+
+// 判断是否满足红黑树性质2：根节点是黑色
+bool judge2(node* root) {
+    if (root == NULL)
+        return true;
     int l = getNum(root->left);
     int r = getNum(root->right);
-    if(l != r) return false;
+    if (l != r)
+        return false;
     return judge2(root->left) && judge2(root->right);
 }
+
 int main() {
     int k, n;
     scanf("%d", &k);
+
     for (int i = 0; i < k; i++) {
         scanf("%d", &n);
         arr.resize(n);
         node* root = NULL;
+
+        // 读取输入数据，构建二叉搜索树
         for (int j = 0; j < n; j++) {
             scanf("%d", &arr[j]);
             root = build(root, arr[j]);
         }
+
+        // 判断是否满足红黑树性质
         if (arr[0] < 0 || judge1(root) == false || judge2(root) == false)
             printf("No\n");
         else
