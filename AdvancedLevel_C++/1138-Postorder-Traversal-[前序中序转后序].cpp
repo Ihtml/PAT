ea3bea62496fb4fbb4c0f11469622695d7dbf807
Ihtml <2483690020@qq.com>
2023-10-25 23:30:55 +0800

    feat(pat-a-1138): postorder

--- AdvancedLevel_C++/1138-Postorder-Traversal-[前序中序转后序].cpp
+++ AdvancedLevel_C++/1138-Postorder-Traversal-[前序中序转后序].cpp
@@ -4,6 +4,19 @@
 using namespace std;
 vector<int> pre, in;
 bool flag = false;
+void postOrder(int prel, int inl, int inr) {
+    if (inl > inr || flag == true)
+        return;
+    int i = inl;
+    while (in[i] != pre[prel])
+        i++;
+    postOrder(prel + 1, inl, i - 1);
+    postOrder(prel + i - inl + 1, i + 1, inr);
+    if (flag == false) {
+        printf("%d", in[i]);
+        flag = true;
+    }
+}
 int main() {
     int n;
     cin >> n;
