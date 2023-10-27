89aed1412be1843f99a46f878c38c487edcf32a7
Ihtml <2483690020@qq.com>
2023-10-27 23:35:00 +0800

    feat(pat-a-1139): init commit

---
+++ AdvancedLevel_C++/1139-First-Contact-[模拟].cpp
@@ -0,0 +1,13 @@
+/*分析：1.用数组arr标记两个人是否是朋友（邻接矩阵表示），用v标记所有人的同性朋友（邻接表表示）
+2.对于一对想要在一起的A和B，他们需要先找A的所有同性朋友C，再找B的所有同性朋友D，当C和D两人是朋友的时候则可以输出C和D*/
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+int main() {
+    int n, m, k;
+    scanf("%d%d", &n, &m);
+    vector<int> v[10000];
+    return 0;
+}
