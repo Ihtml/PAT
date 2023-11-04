5dc413bbaffaf1e86e37a10d1a21432a45ea6efe
Ihtml <2483690020@qq.com>
2023-11-05 00:03:49 +0800

    feat(pat-a-1142): init

---
+++ AdvancedLevel_C++/1142-Maximal-Clique-[无向完全图].cpp
@@ -0,0 +1,9 @@
+/*判断是否是极大连通分量，即判断是否任意两边都相连；
+之后判断是否是maximal，即遍历所有不在集合中的剩余的点，看是否存在一个点满足和集合中所有的结点相连，
+最后如果都满足，那就输出Yes表示是Maximal clique */
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    return 0;
+}
