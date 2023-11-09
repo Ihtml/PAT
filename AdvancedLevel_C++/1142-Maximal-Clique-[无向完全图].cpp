8416b8a7b6681041a59b8df5c38533323596a3e4
Ihtml <2483690020@qq.com>
2023-11-09 08:16:04 +0800

    feat(pat-a-1142): update

--- AdvancedLevel_C++/1142-Maximal-Clique-[无向完全图].cpp
+++ AdvancedLevel_C++/1142-Maximal-Clique-[无向完全图].cpp
@@ -32,6 +32,24 @@ int main() {
                 }
             }
         }
+        if (isclique == 0)
+            continue;
+        for (int j = 1; j <= nv; j++) {
+            if (hash[j] == 0) {
+                for (int l = 0; l < k; l++) {
+                    if (e[v[l]][j] == 0)
+                        break;
+                    if (l == k - 1)
+                        isMaximal = 0;
+                }
+            }
+            if (isMaximal == 0) {
+                printf("Not Maximal\n");
+                break;
+            }
+        }
+        if (isMaximal == 1)
+            printf("Yes\n");
     }
     return 0;
 }
