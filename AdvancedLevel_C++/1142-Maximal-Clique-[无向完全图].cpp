cccb7d33df129ee011baf300e18bd9637e936d2a
Ihtml <2483690020@qq.com>
2023-11-10 09:14:42 +0800

    feat(pat-a-1142): add notes

--- AdvancedLevel_C++/1142-Maximal-Clique-[无向完全图].cpp
+++ AdvancedLevel_C++/1142-Maximal-Clique-[无向完全图].cpp
@@ -4,23 +4,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int e[210][210];
+
+int e[210][210];  // 邻接矩阵，用于表示图的边关系
+
 int main() {
     int nv, ne, m, ta, tb, k;
-    cin >> nv >> ne;
+    cin >> nv >> ne;  // 输入顶点数和边数
+
+    // 构建邻接矩阵
     for (int i = 0; i < ne; i++) {
         cin >> ta >> tb;
-        e[ta][tb] = e[tb][ta] = 1;
+        e[ta][tb] = e[tb][ta] = 1;  // 标记顶点 ta 和 tb 之间有边相连
     }
-    cin >> m;
+
+    cin >> m;  // 输入测试集合的数量
     for (int i = 0; i < m; i++) {
-        cin >> k;
-        vector<int> v(k);
+        cin >> k;          // 输入当前集合中的顶点数量
+        vector<int> v(k);  // 存储当前集合的顶点
+
         int hash[210] = {0}, isclique = 1, isMaximal = 1;
+
+        // 标记当前集合中的顶点
         for (int j = 0; j < k; j++) {
             scanf("%d", &v[j]);
             hash[v[j]] = 1;
         }
+
+        // 判断当前集合是否是一个团（任意两个顶点都相连）
         for (int j = 0; j < k; j++) {
             if (isclique == 0)
                 break;
@@ -32,8 +42,11 @@ int main() {
                 }
             }
         }
+
         if (isclique == 0)
             continue;
+
+        // 判断当前集合是否是极大团（不能再添加任何一个顶点使其成为团）
         for (int j = 1; j <= nv; j++) {
             if (hash[j] == 0) {
                 for (int l = 0; l < k; l++) {
@@ -48,8 +61,11 @@ int main() {
                 break;
             }
         }
+
+        // 输出结果
         if (isMaximal == 1)
             printf("Yes\n");
     }
+
     return 0;
 }
