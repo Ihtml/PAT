14cce9227b4c0b5c9607ecf90e92309a10de5a35
Ihtml <2483690020@qq.com>
2023-11-15 23:18:15 +0800

    feat(pat-a-1143): init commit

---
+++ AdvancedLevel_C++/1143-Lowest-Common-Ancestor-[].cpp
@@ -0,0 +1,12 @@
+// 给出一棵二叉搜索树的前序遍历，问结点u和v的共同最低祖先是谁
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+map<int, bool> mp;
+int main(){
+    int m, n, u, v, a;
+    scanf("%d %d", &m, &n);
+    vector<int> pre(n);
+    return 0;
+}
