bf2e3ade4287aadea598b7fe28a82ca1ad126d61
Ihtml <2483690020@qq.com>
2023-11-19 23:12:36 +0800

    feat(pat-a-1143): add notes

--- AdvancedLevel_C++/1143-Lowest-Common-Ancestor-[].cpp
+++ AdvancedLevel_C++/1143-Lowest-Common-Ancestor-[].cpp
@@ -3,22 +3,36 @@
 #include <map>
 #include <vector>
 using namespace std;
+
+// 使用 map 记录每个节点是否存在
 map<int, bool> mp;
-int main(){
+
+int main() {
     int m, n, u, v, a;
+
+    // 输入节点数量和查询次数
     scanf("%d %d", &m, &n);
+
+    // 保存前序遍历结果
     vector<int> pre(n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &pre[i]);
-        mp[pre[i]] = true;
+        mp[pre[i]] = true; // 标记节点存在
     }
+
+    // 处理每一次查询
     for (int i = 0; i < m; i++) {
         scanf("%d %d", &u, &v);
+
+        // 寻找最低祖先
         for (int j = 0; j < n; j++) {
             a = pre[j];
+            // 如果 a 在 u 和 v 之间，则 a 就是最低祖先
             if ((a >= u && a <= v) || (a >= v && a <= u))
                 break;
         }
+
+        // 输出结果
         if (mp[u] == false && mp[v] == false)
             printf("ERROR: %d and %d are not found.\n", u, v);
         else if (mp[u] == false || mp[v] == false)
@@ -28,5 +42,6 @@ int main(){
         else
             printf("LCA of %d and %d is %d.\n", u, v, a);
     }
+
     return 0;
 }
