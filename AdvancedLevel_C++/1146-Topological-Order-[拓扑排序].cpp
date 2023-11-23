8d4027d73867db610c0d119240f2c8ec4e9ce537
Ihtml <2483690020@qq.com>
2023-11-23 23:36:20 +0800

    feat(pat-a-1146): add notes

--- AdvancedLevel_C++/1146-Topological-Order-[拓扑排序].cpp
+++ AdvancedLevel_C++/1146-Topological-Order-[拓扑排序].cpp
@@ -1,7 +1,11 @@
 // 给一个有向图，判断给定序列是否是拓扑序列
+// 用邻接表v存储这个有向图，并将每个节点的入度保存在in数组中。对每一个要判断是否是拓扑序列的结点遍历，
+// 如果当前结点的入度不为0则表示不是拓扑序列，每次选中某个点后要将它所指向的所有结点的入度 - 1
+// 最后根据是否出现过入度不为0的点决定是否要输出当前的编号i,flag是用来判断之前是否输出过现在是否要输出空格的
+// judge是用来判断是否是拓扑序列
 #include <iostream>
 #include <vector>
-using namespace std;
+    using namespace std;
 int main() {
     int n, m, a, b, k, flag = 0, in[1010];
     vector<int> v[1010];
