5f12cca16b8166d247de268755bf5c99d8033386
Ihtml <2483690020@qq.com>
2023-12-06 23:37:20 +0800

    feat(pat-a-1150): add notes

--- AdvancedLevel_C++/1150-Travelling-Salesman-Problem-[图].cpp
+++ AdvancedLevel_C++/1150-Travelling-Salesman-Problem-[图].cpp
@@ -5,50 +5,79 @@
 #include <set>
 #include <vector>
 using namespace std;
-int e[300][300], n, m, k, ans = 99999999, ansid;
+
+// 图的邻接矩阵表示
+int e[300][300];
+// 图的顶点数和边数
+int n, m;
+// 待检查的路径数、最短路径和对应的路径编号
+int k, ans = 99999999, ansid;
+// 用于存储读取路径的顶点序列
 vector<int> v;
+
+// 检查路径是否为旅行商环路
 void check(int index) {
     int sum = 0, cnt, flag = 1;
+    // 读取路径长度
     scanf("%d", &cnt);
     set<int> s;
     vector<int> v(cnt);
+
+    // 读取路径中的顶点
     for (int i = 0; i < cnt; i++) {
         scanf("%d", &v[i]);
         s.insert(v[i]);
     }
+
+    // 检查路径的连通性
     for (int i = 0; i < cnt - 1; i++) {
         if (e[v[i]][v[i + 1]] == 0)
             flag = 0;
         sum += e[v[i]][v[i + 1]];
     }
+
+    // 输出检查结果
     if (flag == 0) {
         printf("Path %d: NA (Not a TS cycle)\n", index);
     } else if (v[0] != v[cnt - 1] || s.size() != n) {
         printf("Path %d: %d (Not a TS cycle)\n", index, sum);
     } else if (cnt != n + 1) {
         printf("Path %d: %d (TS cycle)\n", index, sum);
+        // 更新最短路径和对应的编号
         if (sum < ans) {
             ans = sum;
             ansid = index;
         }
     } else {
         printf("Path %d: %d (TS simple cycle)\n", index, sum);
+        // 更新最短路径和对应的编号
         if (sum < ans) {
             ans = sum;
             ansid = index;
         }
     }
 }
+
 int main() {
+    // 读取图的顶点数和边数
     scanf("%d%d", &n, &m);
+
+    // 读取图的边信息
     for (int i = 0; i < m; i++) {
         int t1, t2, t;
         scanf("%d%d%d", &t1, &t2, &t);
         e[t1][t2] = e[t2][t1] = t;
     }
+
+    // 读取待检查的路径数
     scanf("%d", &k);
+
+    // 对每条路径进行检查
     for (int i = 1; i <= k; i++)
         check(i);
+
+    // 输出最短路径信息
     printf("Shortest Dist(%d) = %d\n", ansid, ans);
+
     return 0;
 }
