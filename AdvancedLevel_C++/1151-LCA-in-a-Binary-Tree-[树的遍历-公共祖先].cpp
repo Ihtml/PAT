d1814c32a7c29f607e99f6395ec557c9e506d499
Ihtml <2483690020@qq.com>
2023-12-07 23:00:10 +0800

    feat(pat-a-1151): init

---
+++ AdvancedLevel_C++/1151-LCA-in-a-Binary-Tree-[树的遍历-公共祖先].cpp
@@ -0,0 +1,12 @@
+// 给出中序序列和先序序列，再给出两个点，求这两个点的最近公共祖先
+
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+map<int, int> pos;
+vector<int> in, pre;
+int main(){
+    int m, n, a, b;
+    return 0;
+}
