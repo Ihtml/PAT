eef22ba1ca1a968d55c34d9f08d9d8d9c9a9a428
Ihtml <2483690020@qq.com>
2023-12-10 12:04:42 +0800

    feat(pat-a-1151): ac

--- AdvancedLevel_C++/1151-LCA-in-a-Binary-Tree-[树的遍历-公共祖先].cpp
+++ AdvancedLevel_C++/1151-LCA-in-a-Binary-Tree-[树的遍历-公共祖先].cpp
@@ -6,6 +6,21 @@
 using namespace std;
 map<int, int> pos;
 vector<int> in, pre;
+void lca(int inl, int inr, int preRoot, int a, int b) {
+    if (inl > inr)
+        return;
+    int inRoot = pos[pre[preRoot]], aIn = pos[a], bIn = pos[b];
+    if (aIn < inRoot && bIn < inRoot)
+        lca(inl, inRoot - 1, preRoot + 1, a, b);
+    else if ((aIn < inRoot && bIn > inRoot) || (aIn > inRoot && bIn < inRoot))
+        printf("LCA of %d and %d is %d.\n", a, b, in[inRoot]);
+    else if (aIn > inRoot && bIn > inRoot)
+        lca(inRoot + 1, inr, preRoot + 1 + (inRoot - inl), a, b);
+    else if (aIn == inRoot)
+        printf("%d is an ancestor of %d.\n", a, b);
+    else if (bIn == inRoot)
+        printf("%d is an ancestor of %d.\n", b, a);
+}
 int main(){
     int m, n, a, b;
     scanf("%d %d", &m, &n);
@@ -23,7 +38,7 @@ int main(){
         else if (pos[a] == 0 || pos[b] == 0)
             printf("ERROR: %d is not found.\n", pos[a] == 0 ? a : b);
         else
-            // lca(1, n, 1, a, b);
+            lca(1, n, 1, a, b);
     }
     return 0;
 }
