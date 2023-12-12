98dc4d3308557f077a05a7aa78a423269baa2933
Ihtml <2483690020@qq.com>
2023-12-12 10:20:11 +0800

    feat(pat-a-1153): init

---
+++ AdvancedLevel_C++/1153-Decode-Registration-Card-of-PAT-[25]-[模拟排序map].cpp
@@ -0,0 +1,21 @@
+/* 给出一组学生的准考证号和成绩，准考证号包含了等级(
+    乙甲顶)，考场号，日期，和个人编号信息，并有三种查询方式
+    查询一：给出考试等级，找出该等级的考生，按照成绩降序，准考证升序排序
+    查询二：给出考场号，统计该考场的考生数量和总得分
+    查询三：给出考试日期，查询改日期下所有考场的考试人数，按照人数降序，考场号升序排序
+    */
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+    using namespace std;
+struct node {
+    string t;
+    int value;
+};
+int main(){
+    int n, k, num;
+    string s;
+    cin >> n >> k;
+    return 0;
+}
