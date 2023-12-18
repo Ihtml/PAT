6f7f801d9abd17c97c657c90a01777fc349e72d1
Ihtml <2483690020@qq.com>
2023-12-18 23:10:10 +0800

    feat(pat-a-1154): init

---
+++ AdvancedLevel_C++/1154-Vertex-Coloring-[图-set-hash].cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+struct node {
+    int t1, t2;
+};
+int main() {
+    int n, m, k;
+    cin >> n >> m;
+    vector<node> v(m);
+    return 0;
+}
