7b5876a8c17356e29c878f3b44e8a2f6589f250f
Ihtml <2483690020@qq.com>
2023-12-19 23:19:01 +0800

    feat(pat-a-1154): update

--- AdvancedLevel_C++/1154-Vertex-Coloring-[图-set-hash].cpp
+++ AdvancedLevel_C++/1154-Vertex-Coloring-[图-set-hash].cpp
@@ -1,3 +1,9 @@
+/*
+Q:给出一个图（先给出所有边，后给出每个点的颜色），
+问是否满足：所有的边的两个点的颜色不相同
+A:把所有边存起来，把所有点的颜色存起来（存的过程中放入set统计颜色个数），
+枚举所有边
+*/
 #include <iostream>
 #include <set>
 #include <vector>
@@ -9,5 +15,8 @@ int main() {
     int n, m, k;
     cin >> n >> m;
     vector<node> v(m);
+    for (int i = 0; i < m; i++)
+        scanf("%d %d", &v[i].t1, &v[i].t2);
+    cin >> k;
     return 0;
 }
