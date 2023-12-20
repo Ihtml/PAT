85bc4b6cd836e4bd009c5b14167e253023d32e40
Ihtml <2483690020@qq.com>
2023-12-20 20:25:25 +0800

    feat(pat-a-1154): update

--- AdvancedLevel_C++/1154-Vertex-Coloring-[图-set-hash].cpp
+++ AdvancedLevel_C++/1154-Vertex-Coloring-[图-set-hash].cpp
@@ -2,7 +2,7 @@
 Q:给出一个图（先给出所有边，后给出每个点的颜色），
 问是否满足：所有的边的两个点的颜色不相同
 A:把所有边存起来，把所有点的颜色存起来（存的过程中放入set统计颜色个数），
-枚举所有边
+枚举所有边检查是否每条边的两点个颜色是否相同，若全不相同，则输出颜色个数，否则输出No
 */
 #include <iostream>
 #include <set>
@@ -18,5 +18,14 @@ int main() {
     for (int i = 0; i < m; i++)
         scanf("%d %d", &v[i].t1, &v[i].t2);
     cin >> k;
+    while (k--) {
+        int a[10009] = {0};
+        bool flag = true;
+        set<int> se;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+            se.insert(a[i]);
+        }
+    }
     return 0;
 }
