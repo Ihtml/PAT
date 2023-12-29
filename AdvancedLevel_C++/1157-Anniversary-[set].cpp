d12b7892ced20d7753fa8061db436cec2365c58e
Ihtml <2483690020@qq.com>
2023-12-29 22:30:30 +0800

    feat(pat-a-1157): update

--- AdvancedLevel_C++/1157-Anniversary-[set].cpp
+++ AdvancedLevel_C++/1157-Anniversary-[set].cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <set>
 using namespace std;
-int main(){
-    int n, m;
+int main() {
+    int n, m, ctn;
+    string temp, smallx = "99999999", smalla = "99999999", ansx, ansa;
+    set<string> record;
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        record.insert(temp);
     }
     cin >> m;
-    for (int i = 0; i < m; i++)
-    {
-        
+    for (int i = 0; i < m; i++) {
+        cin >> temp;
+        if (record.count(temp)) {
+            ctn++;
+        }
     }
-    
+
     return 0;
 }
