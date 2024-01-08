5b5cb6ee5bfbda376751bd91890cf29910f9909f
Ihtml <2483690020@qq.com>
2024-01-08 15:25:11 +0800

    feat(pat-a-1159): deal function

--- AdvancedLevel_C++/1159-Structure-of-a-Binary-Tree-[中序后序遍历序列确定二叉树].cpp
+++ AdvancedLevel_C++/1159-Structure-of-a-Binary-Tree-[中序后序遍历序列确定二叉树].cpp
@@ -7,6 +7,23 @@ struct node {
 int n, m, a, b, root, f, Full;
 int In[32], Post[32];
 string t;
+int deal(int R, int start, int end, int Pa) {
+    if (start > end) {
+        return -1;
+    }
+    int i = start;
+    while (i < end && In[i] != Post[R]) {
+        i++;
+    }
+    Tree[Post[R]].parent = Pa;
+    Tree[Post[R]].level = Tree[Pa].level + 1;
+    Tree[Post[R]].lchild = deal(R - 1 - end + i, start, i - 1, Post[R]);
+    Tree[Post[R]].rchild = deal(R - 1, i + 1, end, Post[R]);
+    if (Tree[Post[R]].lchild * Tree[Post[R]].rchild < 0) {
+        Full = 1;
+    }
+    return Post[R];
+}
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
@@ -16,5 +33,6 @@ int main() {
         cin >> In[i];
     }
     root = Post[n - 1];
+    deal(n - 1, 0, n - 1, 1001);
     return 0;
 }
