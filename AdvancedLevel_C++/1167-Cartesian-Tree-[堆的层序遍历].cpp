e5689bf168c4af434168deec46083848915acc56
Ihtml <2483690020@qq.com>
2024-01-19 22:48:32 +0800

    feat(pat-a-1167): ac

--- AdvancedLevel_C++/1167-Cartesian-Tree-[堆的层序遍历].cpp
+++ AdvancedLevel_C++/1167-Cartesian-Tree-[堆的层序遍历].cpp
@@ -14,17 +14,24 @@ void deal(int index, int L, int R) {
     int pos = L;
     for (int i = L + 1; i <= R; i++) {
         if (In[i] < In[pos]) {
-            pos = i;
+            pos = i;  // 在中序遍历中找到最小的那个数作为当前的根结点
         }
     }
     ans[index] = In[pos];
-    deal(index << 1, L, pos - 1);
-    deal(index << 1 | 1, pos + 1, R);
+    deal(index << 1, L, pos - 1);      // index * 2
+    deal(index << 1 | 1, pos + 1, R);  // (index * 2) + 1
 }
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> In[i];
+    }
+    deal(1, 0, n - 1);
+    for (auto it : ans) {
+        if (it.first != 1) {
+            cout << " ";
+        }
+        cout << it.second;
     }
     return 0;
 }
