82f82c6d1d2e527556a60364e870dcac3f8e741d
Ihtml <2483690020@qq.com>
2024-02-04 20:47:00 +0800

    feat(bfs): update

---
+++ 2024/BFS/demo1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+void BFS(int s) {
+    queue<int> q;
+    q.push(s);
+    while (!q.empty()) {
+        // 取出队首元素pop
+        // 访问队首元素top
+        // 将队首元素出队
+        // 将top的下一层结点中未曾入队的结点全部入队，并设置为已入队
+    }
+}
