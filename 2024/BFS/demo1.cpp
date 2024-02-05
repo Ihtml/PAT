5d556f2a2da07ab8b5ac5bbd435ee9f1aa0994d1
Ihtml <2483690020@qq.com>
2024-02-05 22:30:21 +0800

    feat(bfs): update BST layerOrder

--- 2024/BFS/demo1.cpp
+++ 2024/BFS/demo1.cpp
@@ -11,3 +11,25 @@ void BFS(int s) {
         // 将top的下一层结点中未曾入队的结点全部入队，并设置为已入队
     }
 }
+struct node {
+    int data;
+    node* lchild;
+    node* rchild;
+};
+
+//  二叉树层序遍历
+void LayerOrder(node* root) {
+    queue<node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        node* now = q.front();
+        q.pop();
+        cout << now->data;
+        if (now->lchild != NULL) {
+            q.push(now->lchild);
+        }
+        if (now->rchild != NULL) {
+            q.push(now->rchild);
+        }
+    }
+}
