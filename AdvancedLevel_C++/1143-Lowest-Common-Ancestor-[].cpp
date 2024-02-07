4870e8a9d2df87cf8d945bed04e59c60b09387ab
Ihtml <2483690020@qq.com>
2024-02-07 22:10:33 +0800

    feat(pat-a-1143): update note

--- AdvancedLevel_C++/1143-Lowest-Common-Ancestor-[].cpp
+++ AdvancedLevel_C++/1143-Lowest-Common-Ancestor-[].cpp
@@ -10,14 +10,14 @@ map<int, bool> mp;
 int main() {
     int m, n, u, v, a;
 
-    // 输入节点数量和查询次数
+    // 输入查询次数和节点数量
     scanf("%d %d", &m, &n);
 
     // 保存前序遍历结果
     vector<int> pre(n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &pre[i]);
-        mp[pre[i]] = true; // 标记节点存在
+        mp[pre[i]] = true;  // 标记节点存在
     }
 
     // 处理每一次查询
@@ -27,7 +27,8 @@ int main() {
         // 寻找最低祖先
         for (int j = 0; j < n; j++) {
             a = pre[j];
-            // 如果 a 在 u 和 v 之间，则 a 就是最低祖先
+            // 先序遍历的二叉搜索树中，根据“根左右”原则，如果 a 在 u 和 v
+            // 之间，则 a 就是最低祖先
             if ((a >= u && a <= v) || (a >= v && a <= u))
                 break;
         }
