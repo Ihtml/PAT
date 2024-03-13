b2a2db036773a916ccf466d9cba990ee948b3f82
Ihtml <2483690020@qq.com>
2024-03-13 17:52:46 +0800

    feat(graph): prim

--- 2024/Graph/Prim.cpp
+++ 2024/Graph/Prim.cpp
@@ -31,8 +31,30 @@ void Dijkstra(int s) {  // s为起点
         }
     }
 }
-int prim() {
-    // 默认0号为初始点，函数返回最小生成树的边权之和
+int prim() {  // 默认0号为初始点，函数返回最小生成树的边权之和
+    fill(d, d + MAXV * MAXV, INF);
+    d[0] = 0;     // 只有0号顶点到集合s的距离为0，其余全为INF
+    int ans = 0;  // 存放最小生成树的边权之和
+    for (int i = 0; i < n; i++) {
+        int u = -1, MIN = INF;  // u使d[u]最小，MIN存放改最小的d[u]
+        for (int j = 0; j < n; j++) {  // 找到未访问的顶点中d[]最小的
+            if (vis[j] == false && d[j] < MIN) {
+                u = j;
+                MIN = d[j];
+            }
+        }
+        if (u == -1) {
+            return;
+        }
+        vis[u] = true;
+        ans += d[u];  // 将与集合s距离最近的边加入最小生成树
+        for (int v = 0; v < n; v++) {  // 以u为中介点可以使v离集合s更近
+            if (vis[v] == false && G[u][v] != INF && G[u][v] < d[v]) {
+                d[v] = G[u][v];  // 将G[u][v]赋值给d[v]
+            }
+        }
+    }
+    return ans;  // 返回最小生成树的边权之和
 }
 int main() {
     cin >> n >> m;
